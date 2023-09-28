3f4eb53fd4d1f04a9a2ef842eeb8532e84d53eea
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2023-09-27 23:57:07 -0300

    configurando api-key

--- Patterns/StatusUpdates/ClientPolling/apigateway.tf
+++ Patterns/StatusUpdates/ClientPolling/apigateway.tf
@@ -19,8 +19,21 @@ resource "aws_api_gateway_method" "api_gateway_orders_method_post" {
   resource_id   = aws_api_gateway_resource.api_gateway_orders_resource.id
   http_method   = "POST"
   authorization = "NONE"  # Use "CUSTOM" para habilitar API Key
+  #authorizer_id = aws_api_gateway_authorizer.order_authorizer.id
 
+  request_parameters = {
+    "method.request.header.x-api-key" = true
+  }
 }
+
+/*
+resource "aws_api_gateway_authorizer" "order_authorizer" {
+  name          = "order-authorizer"
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway_orders.id
+  type          = "TOKEN"
+  identity_source = "method.request.header.x-api-key"
+}
+*/
 
 # Crie uma integração AWS Service do tipo SQS
 resource "aws_api_gateway_integration" "api_gateway_order_integration_order" {
@@ -48,13 +61,6 @@ resource "aws_api_gateway_deployment" "gateway_order_deployment_orders" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
 
   triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.api_gateway_orders_resource.id,
       aws_api_gateway_method.api_gateway_orders_method_post.id,
@@ -67,31 +73,59 @@ resource "aws_api_gateway_deployment" "gateway_order_deployment_orders" {
   }
 }
 
-resource "aws_api_gateway_stage" "api_gateway_stage_dev" {
+resource "aws_api_gateway_stage" "order_development" {
   deployment_id = aws_api_gateway_deployment.gateway_order_deployment_orders.id
   rest_api_id   = aws_api_gateway_rest_api.api_gateway_orders.id
   stage_name    = "dev"
 }
+
+
+resource "aws_api_gateway_api_key" "order_api_key" {
+  name    = "order-api-key"
+  description = "An example API key created with Terraform"
+  enabled = true
+}
+
+resource "aws_api_gateway_usage_plan" "order_usage_plan" {
+  name         = "order-plan"
+  description  = "my description"
+  product_code = "MYCODE"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.api_gateway_orders.id
+    stage  = aws_api_gateway_stage.order_development.stage_name
+  }
+
+  quota_settings {
+    limit  = 20
+    offset = 2
+    period = "WEEK"
+  }
+
+  throttle_settings {
+    burst_limit = 5
+    rate_limit  = 10
+  }
+}
+
+
+resource "aws_api_gateway_usage_plan_key" "order_usage_plan_key" {
+  key_id = aws_api_gateway_api_key.order_api_key.id
+  key_type = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.order_usage_plan.id
+}
+
+
+
+
+
+
 
 resource "aws_api_gateway_method_response" "order_method_response_200" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
   resource_id = aws_api_gateway_resource.api_gateway_orders_resource.id
   http_method = aws_api_gateway_method.api_gateway_orders_method_post.http_method
   status_code = "200"
-}
-
-resource "aws_api_gateway_method_response" "order_method_response_400" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
-  resource_id = aws_api_gateway_resource.api_gateway_orders_resource.id
-  http_method = aws_api_gateway_method.api_gateway_orders_method_post.http_method
-  status_code = "400"
-}
-
-resource "aws_api_gateway_method_response" "order_method_response_500" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
-  resource_id = aws_api_gateway_resource.api_gateway_orders_resource.id
-  http_method = aws_api_gateway_method.api_gateway_orders_method_post.http_method
-  status_code = "500"
 }
 
 resource "aws_api_gateway_integration_response" "api_gateway_integration_response_200" {
@@ -103,19 +137,15 @@ resource "aws_api_gateway_integration_response" "api_gateway_integration_respons
 
   # Transforms the backend JSON response to XML
   response_templates = {
-    "application/json" = <<EOT
-        #set($inputRoot = $input.path('$'))
-        {
-          "statusCode": 200,
-          "message": "Resposta personalizada com dados do SQS",
-          "data":{
-            "requestId": \"$inputRoot.SendMessageResponse.ResponseMetadata.RequestId\",
-            "messageId": \"$inputRoot.SendMessageResponse.SendMessageResult.MessageId\",
-            "sequenceNumber": \"$inputRoot.SendMessageResponse.SendMessageResult.SequenceNumber\"
-          } 
-        }
-    EOT
+    "application/json" = ""
   }
+}
+
+resource "aws_api_gateway_method_response" "order_method_response_400" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
+  resource_id = aws_api_gateway_resource.api_gateway_orders_resource.id
+  http_method = aws_api_gateway_method.api_gateway_orders_method_post.http_method
+  status_code = "400"
 }
 
 resource "aws_api_gateway_integration_response" "api_gateway_integration_response_400" {
@@ -131,6 +161,13 @@ resource "aws_api_gateway_integration_response" "api_gateway_integration_respons
   }
 }
 
+resource "aws_api_gateway_method_response" "order_method_response_500" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
+  resource_id = aws_api_gateway_resource.api_gateway_orders_resource.id
+  http_method = aws_api_gateway_method.api_gateway_orders_method_post.http_method
+  status_code = "500"
+}
+
 resource "aws_api_gateway_integration_response" "api_gateway_integration_response_500" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
   resource_id = aws_api_gateway_resource.api_gateway_orders_resource.id
@@ -143,4 +180,3 @@ resource "aws_api_gateway_integration_response" "api_gateway_integration_respons
     "application/json" = ""
   }
 }
-
