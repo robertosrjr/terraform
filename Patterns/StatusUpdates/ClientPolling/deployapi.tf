fe53d3db76c9ced4c816158c0fe93ba8eddf1043
Roberto Silva Ramos Junior <robertosrjr@gmail.com>
2025-10-29 19:56:17 -0300

    restruturacao

--- Patterns/StatusUpdates/ClientPolling/deployapi.tf
+++
@@ -1,7 +0,0 @@
-/*
-resource "aws_api_gateway_deployment" "deployment_stage_dev" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_orders.id
-  stage_name = "dev"
-  description = "Deployment de desenvolvimento"
-}
-*/
